ac1451fe7c70d4dcc3662a33bac4f21f774eab24
zsc <2827636950@qq.com>
2025-05-10 12:15:59 +0800

    计算G-mean和MAUC，评估集成分类性能

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
-from sklearn.metrics import confusion_matrix
+from scipy.stats import gmean
+from sklearn.metrics import confusion_matrix, roc_auc_score
 
 
 def calculate_expert_accuracy(y_pred, y, weights):
@@ -12,8 +13,29 @@ def calculate_expert_accuracy(y_pred, y, weights):
     '''
     cm = confusion_matrix(y, y_pred)
     tp_per_class = cm.diagonal()  # 对角线元素表示每个类预测正确的个数，对角线求和，即所有预测正确的实例个数之和，计算Acc1
-    s_per_class = cm.sum(axis=1) # 对每个类求和，即所有预测的实例个数之和，计算Acc2和Acc3
+    s_per_class = cm.sum(axis=1)  # 对每个类求和，即所有预测的实例个数之和，计算Acc2和Acc3
     acc1 = np.sum(tp_per_class) / np.sum(s_per_class)  # Acc1
     acc2 = np.mean(tp_per_class.astype(float) / s_per_class.astype(float))  # Acc2
     acc3 = np.mean((tp_per_class.astype(float) / s_per_class.astype(float)) * weights)  # Acc3
     return round(acc1, 6), round(acc2, 6), round(acc3, 6)
+
+
+def calculate_gmean_mauc(y_pred_proba, y):
+    '''
+    计算G-mean和MAUC
+    :param y_pred_proba:
+    :param y: 真实标签
+    :return: geometric_mean和auc_ovo_macro
+    '''
+    num_classes = len(np.unique(y))  # 类别数量
+    # 计算auc_ovo_macro
+    if num_classes == 2:
+        auc_ovo_macro = roc_auc_score(y, y_pred_proba[:, 1])
+    else:
+        auc_ovo_macro = roc_auc_score(y, y_pred_proba, multi_class="ovo", average="macro")
+    y_pred = np.argmax(y_pred_proba, axis=1)
+    cm = confusion_matrix(y, y_pred)
+
+    recall_per_class = cm.diagonal() / cm.sum(axis=1)  # 计算每类召回率（每类正确预测个数 / 该类总数）
+    geometric_mean = gmean(recall_per_class)  # geometric_mean
+    return round(geometric_mean, 6), round(auc_ovo_macro, 6)
--- mile/mile.py
+++ mile/mile.py
@@ -1,8 +1,9 @@
 from sklearn.base import clone
 from sklearn.neural_network import MLPClassifier
 import random
-from config import BalanceScale
-from evolutionary_operator import init_toolbox
+from config import BalanceScale, MILEConfig
+from evolutionary_operator import init_toolbox, ensemble_individuals, vote_result_ensembles
+from metrics import calculate_gmean_mauc
 from utils import pre_processing
 
 
@@ -35,57 +36,90 @@ class MILE():
         self.toolbox = init_toolbox(self.estimator, self.x_train, self.y_train, self.weights_train, self.constraints,
                                     self.random_state, self.n_splits)
         self.parameter = parameter
+        self.ensemble_classifiers = None
 
-    def fit(self, ):
-        ####################################种群的初始化###########################
-        pop = self.toolbox.population(n=self.parameter.POPSIZE)  # 个体编码默认全为0
+    def fit(self):
+        '''
+        实例子集的搜索，获取分类器集合
+        :return: void
+        '''
+        # 种群的初始化
+        pop = self.toolbox.population(n=self.parameter.POPSIZE)
         pop = self.toolbox.init_pop(pop)  # 初始化种群中的个体
-        self.toolbox.evaluate(pop)  # 计算个体的适应度
-        ####################################种群的迭代#################################################
+        self.toolbox.evaluate(pop)  # 评估初始种群
+        # 实例子集进化搜索
         for gen in range(0, self.parameter.NGEN):
-            offspring = self.toolbox.selTournamentNDCD(pop, self.parameter.POPSIZE, tournsize=3)  # 锦标赛选择（1、先根据非支配排序的等级2、再根据拥挤距离）
+            # 选择、交叉、变异
+            offspring = self.toolbox.selTournamentNDCD(pop, self.parameter.POPSIZE, tournsize=3)  # 1、先由非支配排序的等级 2、再由PFC
             offspring = [self.toolbox.clone(ind) for ind in offspring]
             for i in range(0, len(offspring) - 1, 2):
                 if random.random() <= self.parameter.CXPB:
                     offspring[i], offspring[i + 1] = self.toolbox.mate(offspring[i], offspring[i + 1])  # 单点交叉
-                offspring[i] = self.toolbox.mutate(offspring[i], self.parameter.MR)[0]  # 二进制反转突变
-                offspring[i + 1] = self.toolbox.mutate(offspring[i + 1], self.parameter.MR)[0]  # 二进制反转突变
+                offspring[i] = self.toolbox.mutate(offspring[i], self.parameter.MR)[0]  # 二进制反转
+                offspring[i + 1] = self.toolbox.mutate(offspring[i + 1], self.parameter.MR)[0]  # 二进制反转
                 del offspring[i].fitness.values, offspring[i + 1].fitness.values
-            #############################################################合并、去重#####################################################
-            offspring = self.toolbox.individuals_constraints(offspring)  # 限制每个类至少有一个实例被选择
+            # 合并、去重
             pop = pop + offspring  # 种群的合并
             pop, _ = self.toolbox.remove_duplicates(pop)  # 去重
-            while len(pop) < self.parameter.POPSIZE:  # 保证种群大小为POPSIZE
+            # 保证种群大小为POPSIZE，避免因重复过多而造成的种群数量减少
+            while len(pop) < self.parameter.POPSIZE:
                 add_individual = []
-                num_add = self.parameter.POPSIZE - len(pop)
+                num_add = self.parameter.POPSIZE - len(pop)  # 需要添加的个体数量
+                # 随机选择一个个体进行突变
                 for i in range(0, num_add):
-                    index = random.randint(0, len(offspring) - 1)  # 在0-len(offspring)范围内随机产生一个索引
+                    index = random.randint(0, len(offspring) - 1)
                     offspring[index] = self.toolbox.mutate(offspring[index], self.parameter.MR)[0]  # 选择index对应的个体进行突变
                     del offspring[index].fitness.values
                     add_individual.append(offspring[index])
-                add_individual = self.toolbox.individuals_constraints(add_individual)  # 限制每个类至少有一个实例被选择
-                pop = pop + add_individual  # 种群的合并
-                pop, _ = self.toolbox.remove_duplicates(pop)  # 去重
-            self.toolbox.evaluate(pop)  # 计算新种群适应度
-            ###############################################得到pareto_fronts############################################
-            feasible_pop, infeasible_pop = self.toolbox.get_feasible_infeasible(pop)  # 得到可行解与不可行解
+                pop = pop + add_individual  # 再次合并
+                pop, _ = self.toolbox.remove_duplicates(pop)  # 再次去重
+            self.toolbox.evaluate(pop)  # 评估新种群
+            feasible_pop, infeasible_pop = self.toolbox.get_feasible_infeasible(pop)  # 得到可行个体与不可行个体
             if len(feasible_pop) >= self.parameter.POPSIZE:
                 pop = self.toolbox.select(feasible_pop, self.parameter.POPSIZE)
-                ensembles = pop  # pop均为可行解，则集成pop中所有个体
+                ensembles = pop  # pop均为可行个体，则集成pop中所有个体
             elif len(feasible_pop) > 0:
-                pop = feasible_pop + infeasible_pop[:self.parameter.POPSIZE - len(feasible_pop)]  # 在不可行解中选取违约程度小的个体，保证pop数量为POPSIZE
-                ensembles = feasible_pop  # 只集成可行解
+                pop = feasible_pop + infeasible_pop[
+                                     :self.parameter.POPSIZE - len(feasible_pop)]  # 在不可行个体中选取违约程度小的个体，保证pop数量为POPSIZE
+                ensembles = feasible_pop  # 只集成可行个体
             else:
-                pop = feasible_pop + infeasible_pop[:self.parameter.POPSIZE - len(feasible_pop)]  # 加入不可行解中违约程度小的个体，保证pop数量为POPSIZE
-                ensembles = [infeasible_pop[0]]  # 没有可行解，集成不可行解中第一个个体
-            return ensembles
+                pop = feasible_pop + infeasible_pop[
+                                     :self.parameter.POPSIZE - len(feasible_pop)]  # 加入不可行个体中违约程度小的个体，保证pop数量为POPSIZE
+                ensembles = [infeasible_pop[0]]  # 没有可行个体，集成不可行个体中第一个个体
+            self.ensemble_classifiers = ensemble_individuals(ensembles, self.estimator, self.x_train, self.y_train)
+
+    def predict(self, x_test):
+        '''
+        集成预测
+        :param x_test: 测试集特征数据
+        :return: 集成预测结果
+        '''
+        y_pred = vote_result_ensembles(self.ensemble_classifiers, x_test, result='hard')
+        return y_pred
+
+    def predict_proba(self, x_test):
+        '''
+        集成预测概率
+        :param x_test: 测试集特征数据
+        :return: 集成预测概率
+        '''
+        y_pred_prob = vote_result_ensembles(self.ensemble_classifiers, x_test, result='soft')
+        return y_pred_prob
 
 
 if __name__ == '__main__':
+    mile_parameter = MILEConfig()
     IMBALANCED_DATASET_PATH = '../datasets/mat/'
     DATASET = BalanceScale  # 数据集名称（包含对应的参数配置）
     file_path = IMBALANCED_DATASET_PATH + DATASET.DATASETNAME
     name = BalanceScale.DATASETNAME.split('.')[0]
     mlp = MLPClassifier(hidden_layer_sizes=(DATASET.HIDDEN_SIZE,), max_iter=DATASET.MAX_ITER,
                         random_state=42, learning_rate_init=DATASET.LEARNING_RATE)
-    mile = MILE(file_path=file_path, estimator=mlp, random_state=42)
+
+    mile = MILE(file_path=file_path, estimator=mlp, random_state=42, n_splits=5, display_distribution=True,
+                parameter=mile_parameter)
+
+    mile.fit()
+    y_pred_proba = mile.predict_proba(mile.x_test)
+    gmean, mauc = calculate_gmean_mauc(y_pred_proba, mile.y_test)
+    print(f"Gmean：{gmean}，mAUC：{mauc}")
